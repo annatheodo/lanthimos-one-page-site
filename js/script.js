576c02951dcab6390867086552549a91df9dedfb
Anna Theodoropoulou <annatheodoropoulou@hotmail.com>
2019-08-30 15:16:00 +0200

    Added all project files

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/*Smooth scrolling when clicking an anchor link*/
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+	var height = $(window).scrollTop();
+	if (height > 100) {
+		$('#back2Top').fadeIn();
+	} else {
+		$('#back2Top').fadeOut();
+	}
+});
+$(document).ready(function() {
+	$("#back2Top").click(function(event) {
+		event.preventDefault();
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+		return false;
+	});
+});
+/*Scroll to top when arrow up clicked END*/
